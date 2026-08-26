06d57b7fced5bfab329b921342c062fdbebe8255
luyandakunene73 <kureneluyandaluthando.124284@marwadiuniversity.ac.in>
2026-08-26 22:07:19 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "users_db", 3306);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
--- index.php
+++ index.php
@@ -1,11 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+
+<h2>User Registration</h2>
+
+<form method="POST">
+    Name:
+    <input type="text" name="name" required><br><br>
+
+    Username:
+    <input type="text" name="username" required><br><br>
+
+    Email:
+    <input type="email" name="email" required><br><br>
+
+    Password:
+    <input type="password" name="password" required><br><br>
+
+    <input type="submit" name="register" value="Register">
+</form>
+
 <?php
-if (isset($_COOKIE["visited"])) {
-    $message = "Welcome back! You are a repeated user.";
-} else {
-    setcookie("visited", "yes", time() + (30 * 24 * 60 * 60));
-    $message = "Welcome! You are a new user.";
+if (isset($_POST["register"])) {
+    include "db.php";
+
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users (name, username, email, password)
+            VALUES ('$name', '$username', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "<p>Registration successful!</p>";
+    } else {
+        echo "<p>Error: " . mysqli_error($conn) . "</p>";
+    }
 }
 ?>
 
-<h2>New or Repeated User</h2>
-<p><?php echo $message; ?></p>
+</body>
+</html>
