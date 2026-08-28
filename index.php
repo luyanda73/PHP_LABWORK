140b197942e505795a02e1684d1320bf3a38e9fb
luyandakunene73 <kureneluyandaluthando.124284@marwadiuniversity.ac.in>
2026-08-28 21:48:06 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,47 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Registration</title>
-</head>
-<body>
-
-<h2>User Registration</h2>
-
-<form method="POST">
-    Name:
-    <input type="text" name="name" required><br><br>
-
-    Username:
-    <input type="text" name="username" required><br><br>
-
-    Email:
-    <input type="email" name="email" required><br><br>
-
-    Password:
-    <input type="password" name="password" required><br><br>
-
-    <input type="submit" name="register" value="Register">
-</form>
-
 <?php
-if (isset($_POST["register"])) {
-    include "db.php";
-
+if (isset($_POST["submit"])) {
     $name = $_POST["name"];
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $sql = "INSERT INTO users (name, username, email, password)
-            VALUES ('$name', '$username', '$email', '$password')";
-
-    if (mysqli_query($conn, $sql)) {
-        echo "<p>Registration successful!</p>";
-    } else {
-        echo "<p>Error: " . mysqli_error($conn) . "</p>";
-    }
+    setcookie("username", $name, time() + 3600);
+    echo "Cookie created successfully!";
 }
 ?>
-
-</body>
-</html>
+<h2>Create Cookie in a Form</h2>
+<form method="POST">
+    Enter Name: <input type="text" name="name" required>
+    <input type="submit" name="submit" value="Create Cookie">
+</form>
